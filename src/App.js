1f01f692d7bc7c7b4eff4913f0f002d320ccf625
Sprof22 <richmondelaigwu@gmail.com>
2023-01-19 14:21:16 +0100

    beautiful shii

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import CurrentWeather from './components/CurrentWeather/CurrentWeather';
 import Search from './components/Search/Search';
 
 function App() {
+  const handleSearchChange = (searcData)=> {
+    console.log(searcData);
+  }
   return (
-    <div className="App">
-      <Search />
+    <div className="container">
+      <Search onSearchChange={handleSearchChange}/>
       <CurrentWeather />
     </div>
   );
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AsyncPaginate } from 'react-select-async-paginate'
 
-const Search = () => {
+const Search = ({onSearchChange}) => {
+    const [search, setSearch] = useState(null);
+    const handleOnChange = (searchData) =>{
+        setSearch(searchData);
+        onSearchChange(searchData);
+
+    }
   return (
     <div>
-        hello
+        <AsyncPaginate 
+            placeholder="Search for a City"
+            value={search}
+            onChange={handleOnChange}
+            debounceTimeout={700}
+        />
     </div>
   )
 }
