c67af3d9710ec78c11ce92633d4870572c99a765
Sprof22 <richmondelaigwu@gmail.com>
2023-01-19 14:03:32 +0100

    components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import CurrentWeather from './components/CurrentWeather/CurrentWeather';
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <div className="App">
-      
+      <Search />
+      <CurrentWeather />
     </div>
   );
 }
