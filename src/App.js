f9430d91aea45314f056035e8a554440e60eea94
Sprof22 <richmondelaigwu@gmail.com>
2023-01-23 00:22:52 +0100

    new restart just to make sure

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
 import Search from "./components/Search/Search";
 
 function App() {
-  const handleSearchChange = (searchData) => {
-    console.log(searchData)
-  }
+ 
   return (
     <div className="container">
-      <Search  onSearchChange={handleSearchChange}/>
+      <Search  />
       <CurrentWeather />
     </div>
   );
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,37 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { RapidAPIOptions, RAPID_API_URL } from "../../api";
 
-const Search = ({onSearchChange}) => {
-  const [search, setSearch] = useState(null);
-  const handleInputOnChange = (searchData) => {
-    setSearch(searchData);
-    onSearchChange(searchData);
-  }
-
-  const loadOptions = (inputValue) => {
-  return fetch(`${RAPID_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, RapidAPIOptions)
-	.then(response => response.json())
-	.then(response => {
-    return {
-      options: response.data.map(city => {
-        return {
-          value: `${city.latitude} ${city.longitude}` ,
-          label: `${city.name}, (${city.countryCode})`,
-        }
-      })
-    }
-  })
-	.catch(err => console.error(err));
-  }
+const Search = () => {
+ 
   return (
-    <AsyncPaginate 
-      placeholder={"Search for a city"}
-      debounceTimeout={600}
-      value={search}
-      onChange={handleInputOnChange}
-      loadOptions={loadOptions}
-    />
+    <AsyncPaginate />
   );
 };
 
