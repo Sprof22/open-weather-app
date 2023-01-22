4f4f512d61078a8bc2a781d87e61781a1dc97d78
Sprof22 <richmondelaigwu@gmail.com>
2023-01-22 23:53:02 +0100

    minor to deletes to check pace

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,28 +2,8 @@ import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { RapidAPIOptions, RAPID_API_URL } from "../../api";
 
-const Search = ({ onSearchChange }) => {
-  const [search, setSearch] = useState(null);
-  const handleInputChange = (searchData) => {
-    setSearch(searchData);
-    onSearchChange(searchData);
-  };
-
-  const loadOptions = (searchInput) => {
-    return fetch(`${RAPID_API_URL}/cities?namePrefix=${searchInput}`, RapidAPIOptions)
-	.then(response => response.json())
-	.then(response => {
-        return {
-            options: response.data.map(city => {
-                return {
-                    value: `${city.latitude} ${city.longitude}` ,
-                    label: `${city.name}, ${city.countryCode} (${city.country})`,
-                }
-            })
-        }
-    })
-	.catch(err => console.error(err));
-  };
+const Search = () => {
+ 
   return (
     <AsyncPaginate
       placeholder={"Search for City"}
