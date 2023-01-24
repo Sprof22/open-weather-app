db33d8e066450cfd381f40411e3bb9cf37c8ebc6
Sprof22 <richmondelaigwu@gmail.com>
2023-01-24 17:20:08 +0100

    restarting

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./CurrentWeather.css";
 
 const CurrentWeather = () => {
+  
   return (
-    <div>CurrentWeather</div>
-  )
-}
+    <div className="box">
+      <div className="parent">
+        <div className="display">
+          <div className="first">
+            <div className="temp">18°C</div>
+            <div className="locale">Jamaica</div>
+          </div>
+          <div className="circle" style={{background: `blue`}}></div>
+          <div className="third">
+            <div className="hum">26%</div>
+            <div className="hum">16km/h</div>
+            <div className="hum">6.5h</div>
+          </div>
+        </div>
+        <div className="secondary">
+          gimme some of that
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CurrentWeather
+export default CurrentWeather;
