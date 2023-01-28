ba8f9d991b1233105fc651062327f49398e7503f
Efoneli <abbaefoneli@gmail.com>
2023-01-29 02:55:49 +1030

    api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { OPEN_WEATHER_KEY, OPEN_WEATHER_URL } from "./api";
 import "./App.css";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
 import Search from "./components/Search/Search";
@@ -7,8 +8,14 @@ function App() {
   // const [currentWeather, forecastWeather] = useState(null);
 
   const handleSearchInput = (searchData) => {
-    console.log(searchData)
+    const [lat, lon] = searchData.value.split(' ')
+    const currentWeatherFetch = fetch(`${OPEN_WEATHER_URL}/weather?lat={lat}&lon={lon}&appid=${OPEN_WEATHER_KEY}`)
+    const forecastWeatherFetch = fetch(`${OPEN_WEATHER_URL}/forecast?lat={lat}&lon={lon}&appid=${OPEN_WEATHER_KEY}`)
+
+    Promise.all([currentWeatherFetch, forecastWeatherFetch]).then
   }
+
+
 
  
   return (
