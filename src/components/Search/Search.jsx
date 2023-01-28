73e45941fdf470aa62d2216bd6b6e7615bbc8615
Efoneli <abbaefoneli@gmail.com>
2023-01-29 01:55:37 +1030

    api

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GeoDbApiOptions } from "../../api";
+import { GeoDbApiOptions, GEO_DB_URL } from "../../api";
 
 const Search = ({ onSearchChange }) => {
-
-  const [search, setSearch] = useState(null)
+  const [search, setSearch] = useState(null);
 
   const handleSearchChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
-  }
+  };
 
   const loadOptions = (inputValue) => {
-    return 
-      fetch(`${GEO_DB_URL}/cities`, GeoDbApiOptions)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
-  }
+    return fetch(`${GEO_DB_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, GeoDbApiOptions)
+      .then((response) => response.json())
+      .then((response) => {response.data.map(city => {
+        return {
+          value: `${city.latitude},  ${city.longitude}`,
+          label: `${city.name}, ${city.country}`
+        }
+      })})
+      .catch((err) => console.error(err));
+  };
 
   return (
     <AsyncPaginate
-      placeholder='Search a City'
+      placeholder="Search a City"
       debounceTimeout={600}
       value={search}
       onChange={handleSearchChange}
