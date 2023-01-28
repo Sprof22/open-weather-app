32cbb6461eea523c37cd2da10ef7a68556c2c99f
Sprof22 <richmondelaigwu@gmail.com>
2023-01-28 16:33:13 +0100

    jklkjn

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -12,14 +12,18 @@ const Search = ({ onSearchChange }) => {
   };
 
   const loadOptions = (inputValue) => {
-    return fetch(`${GEO_DB_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, GeoDbApiOptions)
+    return fetch(
+      `${GEO_DB_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      GeoDbApiOptions
+    )
       .then((response) => response.json())
-      .then((response) => {response.data.map(city => {
-        return {
-          value: `${city.latitude},  ${city.longitude}`,
-          label: `${city.name}, ${city.country}`
-        }
-      })})
+      .then((response) => {response.data.map((city) => {
+          return {
+            value: `${city.latitude}` `${city.longitude}`,
+            label: `${city.name}, (${city.country})`,
+          };
+        });
+      })
       .catch((err) => console.error(err));
   };
 
