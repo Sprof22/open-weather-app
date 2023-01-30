6f231a250a03fbb0a4e37a6080942113598f1a75
Efoneli <abbaefoneli@gmail.com>
2023-01-31 07:17:46 +1030

    curr

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+
+
 const CurrentWeather = ({ data }) => {
   const [, kontry] = data.city.split(', ')
   return (
     <div>
-      <img src={`./icons/${data.weather[0].icon}.png`} alt="cloud" width={300} height={200} />
+      <img src={`./icons/${data.weather[0].icon}.png`} alt="cloud" width={100} height={100} />
       <h1>{data.city}</h1>
       <i>
         <h2>{kontry}</h2>
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -19,10 +19,10 @@ const Search = ({ onSearchChange }) => {
       .then((response) => response.json())
       .then((response) => {
         return {
-          options: response.data.map((money) => {
+          options: response.data.map((city) => {
             return {
-              value: `${money.latitude} ${money.longitude}`,
-              label: `${money.name}, (${money.country})`,
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, (${city.country})`,
             };
           }),
         };
