7664979e7dd3571e6081b3544fe6c61e66447eed
Efoneli <abbaefoneli@gmail.com>
2023-02-01 04:36:10 +1030

    accordion

--- src/components/ForecastWeather/ForecastWeather.jsx
+++ src/components/ForecastWeather/ForecastWeather.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
 
-const ForecastWeather = () => {
+const ForecastWeather = ({ data }) => {
   return (
-    <div>ForecastWeather</div>
-  )
-}
+    <div>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.slice(0, 7).map((item, index) => (
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                What harsh truths do you prefer to ignore?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
+                occaecat ut occaecat consequat est minim minim esse tempor
+                laborum consequat esse adipisicing eu reprehenderit enim.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
+};
 
-export default ForecastWeather
+export default ForecastWeather;
