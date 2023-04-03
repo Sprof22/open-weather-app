33f5b7538519986957ace822598edcd457e03cc1
Sprof22 <richmondelaigwu@gmail.com>
2023-04-03 06:16:23 +0100

    push

--- src/components/ForecastWeather/ForecastWeather.jsx
+++ src/components/ForecastWeather/ForecastWeather.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from 'react-accessible-accordion'
 import './ForecastWeather.css'
-const ForecastWeather = () => {
+const ForecastWeather = ({data}) => {
   return (
     <div>
     <label  className="title">Daily</label>
-    <Accordion>
+    <Accordion allowZeroExpanded>
+    {data.list.slice(0,7).map((item, index)=> (
+      <AccordionItem key={item}>
+        <AccordionItemHeading>
+          <AccordionItemButton>
+          <div className="daily-items">
+                  <img
+                    className="icon-small"
+                    src={`icons/01n.png`}
+                    alt="weather"
+                  />
+                  <label htmlFor="" className="day">
+                    Monday
+                  </label>
+                  <label htmlFor="" className="description">
+                    cloudy weather
+                  </label>
+                  <label htmlFor="" className="min-max">
+                    12°C /{" "}
+                    32°C
+                  </label>
+                </div>
+          </AccordionItemButton>
+        </AccordionItemHeading>
+        <AccordionItemPanel>
+          ballalal
+        </AccordionItemPanel>
+      </AccordionItem>
+    ))}
 
     </Accordion>
     
